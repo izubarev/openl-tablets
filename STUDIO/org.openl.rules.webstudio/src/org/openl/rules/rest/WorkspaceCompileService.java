c3358b5e37c1294be212351613fbea40385074e2
Marat Kamalov <mkamalov@eisgroup.com>
2021-03-01 13:15:29 +0300

    EPBDS-10569 EPBDS-10568 update logic for fetching information about compilation process and messages
    --HG--
    branch : EPBDS-10307

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/WorkspaceCompileService.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/WorkspaceCompileService.java
@@ -1,11 +1,13 @@
 package org.openl.rules.rest;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,6 +20,8 @@ import org.openl.message.OpenLMessage;
 import org.openl.rules.project.dependencies.ProjectExternalDependenciesHelper;
 import org.openl.rules.project.instantiation.IDependencyLoader;
 import org.openl.rules.project.model.Module;
+import org.openl.rules.project.model.ProjectDependencyDescriptor;
+import org.openl.rules.project.model.ProjectDescriptor;
 import org.openl.rules.ui.ProjectModel;
 import org.openl.rules.ui.WebStudio;
 import org.openl.rules.webstudio.dependencies.WebStudioWorkspaceRelatedDependencyManager;
@@ -36,59 +40,76 @@ public class WorkspaceCompileService {
 
     @GET
     @Path("progress/{messageId}/{messageIndex}")
-    public Response getCompile(@PathParam("messageId") final Long messageId, @PathParam("messageIndex") final Integer messageIndex) {
+    public Response getCompile(@PathParam("messageId") final Long messageId,
+            @PathParam("messageIndex") final Integer messageIndex) {
         Map<String, Object> compileModuleInfo = new HashMap<>();
         WebStudio webStudio = WebStudioUtils.getWebStudio(WebStudioUtils.getSession());
         if (webStudio != null) {
             ProjectModel model = webStudio.getModel();
             Module moduleInfo = model.getModuleInfo();
-            String projectLoaderName = ProjectExternalDependenciesHelper.buildDependencyNameForProject(moduleInfo.getProject().getName());
-            WebStudioWorkspaceRelatedDependencyManager webStudioWorkspaceDependencyManager = model.getWebStudioWorkspaceDependencyManager();
+            String projectLoaderName = ProjectExternalDependenciesHelper
+                .buildDependencyNameForProject(moduleInfo.getProject().getName());
+            WebStudioWorkspaceRelatedDependencyManager webStudioWorkspaceDependencyManager = model
+                .getWebStudioWorkspaceDependencyManager();
             if (webStudioWorkspaceDependencyManager != null) {
                 int compiledCount = 0;
                 int errorsCount = 0;
                 int warningsCount = 0;
                 int modulesCount = 0;
-                boolean compiled = false;
                 List<MessageDescription> newMessages = new ArrayList<>();
-                List<IDependencyLoader> loaders = webStudioWorkspaceDependencyManager.getDependencyLoaders().values()
-                        .stream().flatMap(Collection::stream).collect(Collectors.toList());
-                for (IDependencyLoader dependencyLoader : loaders) {
-                    if (dependencyLoader == null) {
-                        continue;
-                    }
-                    if (dependencyLoader.isProject()) {
-                        if (!compiled && dependencyLoader.getDependencyName().equals(projectLoaderName) && dependencyLoader.getRefToCompiledDependency() != null) {
-                            compiled = true;
-                        }
-                    } else {
-                        modulesCount++;
-                        CompiledDependency compiledDependency = dependencyLoader.getRefToCompiledDependency();
-                        if (compiledDependency != null) {
-                            for (OpenLMessage message : compiledDependency.getCompiledOpenClass().getMessages()) {
-                                switch (message.getSeverity()){
-                                    case WARN:
-                                        warningsCount++;
-                                        break;
-                                    case ERROR:
-                                        errorsCount++;
-                                        break;
+                Deque<ProjectDescriptor> queue = new ArrayDeque<>();
+                queue.add(moduleInfo.getProject());
+                while (!queue.isEmpty()) {
+                    ProjectDescriptor projectDescriptor = queue.poll();
+                    for (Module module : projectDescriptor.getModules()) {
+                        Collection<IDependencyLoader> dependencyLoadersForModule = webStudioWorkspaceDependencyManager
+                            .getDependencyLoaders()
+                            .get(module.getName());
+                        for (IDependencyLoader dependencyLoader : dependencyLoadersForModule) {
+                            if (dependencyLoader != null) {
+                                CompiledDependency compiledDependency = dependencyLoader.getRefToCompiledDependency();
+                                if (compiledDependency != null) {
+                                    for (OpenLMessage message : compiledDependency.getCompiledOpenClass()
+                                        .getMessages()) {
+                                        switch (message.getSeverity()) {
+                                            case WARN:
+                                                warningsCount++;
+                                                break;
+                                            case ERROR:
+                                                errorsCount++;
+                                                break;
+                                        }
+                                        MessageDescription messageDescription = getMessageDescription(message, model);
+                                        newMessages.add(messageDescription);
+                                    }
+                                    compiledCount++;
                                 }
-                                MessageDescription messageDescription = getMessageDescription(message, model);
-                                newMessages.add(messageDescription);
                             }
-                            compiledCount++;
+                            modulesCount++;
                         }
                     }
-                }
-                if (compiled) {
-                    compiledCount = modulesCount;
+                    for (ProjectDependencyDescriptor pd : projectDescriptor.getDependencies()) {
+                        String projectDependencyName = ProjectExternalDependenciesHelper
+                            .buildDependencyNameForProject(pd.getName());
+                        Collection<IDependencyLoader> dependencyLoadersForProject = webStudioWorkspaceDependencyManager
+                            .getDependencyLoaders()
+                            .get(projectDependencyName);
+                        if (dependencyLoadersForProject != null) {
+                            for (IDependencyLoader dependencyLoader : dependencyLoadersForProject) {
+                                if (dependencyLoader != null && dependencyLoader.isProject()) {
+                                    queue.add(dependencyLoader.getProject());
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
                 compileModuleInfo.put("dataType", "new");
                 if (messageIndex != -1 && messageId != -1) {
                     MessageDescription messageDescription = newMessages.get(messageIndex);
                     if (messageDescription.getId() == messageId) {
-                        newMessages = newMessages.subList(messageIndex + 1, Math.min(MAX_MESSAGES_COUNT - 1, newMessages.size()));
+                        newMessages = newMessages.subList(messageIndex + 1,
+                            Math.min(MAX_MESSAGES_COUNT - 1, newMessages.size()));
                         compileModuleInfo.put("dataType", "add");
                     }
                 }
@@ -96,7 +117,8 @@ public class WorkspaceCompileService {
                 compileModuleInfo.put("modulesCount", modulesCount);
                 compileModuleInfo.put("modulesCompiled", compiledCount);
                 compileModuleInfo.put("messages", newMessages);
-                compileModuleInfo.put("messageId", newMessages.isEmpty() ? -1 : newMessages.get(newMessages.size() - 1).getId());
+                compileModuleInfo.put("messageId",
+                    newMessages.isEmpty() ? -1 : newMessages.get(newMessages.size() - 1).getId());
                 compileModuleInfo.put("messageIndex", newMessages.size() - 1);
                 compileModuleInfo.put("errorsCount", errorsCount);
                 compileModuleInfo.put("warningsCount", warningsCount);
@@ -105,7 +127,7 @@ public class WorkspaceCompileService {
         return Response.ok(compileModuleInfo).build();
     }
 
-    private MessageDescription getMessageDescription(OpenLMessage message, ProjectModel model){
+    private MessageDescription getMessageDescription(OpenLMessage message, ProjectModel model) {
         String url = messageHandler.getSourceUrl(message, model);
         if (StringUtils.isBlank(url)) {
             url = messageHandler.getUrlForEmptySource(message);
@@ -114,4 +136,3 @@ public class WorkspaceCompileService {
     }
 
 }
-
